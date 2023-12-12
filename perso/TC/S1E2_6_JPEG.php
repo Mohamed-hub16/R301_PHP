791c586a3679034fea9582d1ab0217efebe3ec45
Tom CHARPENTIER <127493733+ArkAow@users.noreply.github.com>
2023-12-12 17:49:27 +0000

    correction S01E02_6 + ajout commentaire

--- perso/TC/S1E2_6_JPEG.php
+++ perso/TC/S1E2_6_JPEG.php
@@ -1,17 +1,29 @@
 <?php
-    class ImageGenerator {
-        public static function generateImage() {
-            $image = imagecreatetruecolor(400,200);
-            $blueBackground = imagecolorallocate($image,0,0,255);
-            imagefill($image,0,0, $blueBackground);
+class ImageGenerator { // creation de la classe
+    public static function generateImage() { // creation de la methode
+        header('Content-Type: image/jpeg'); // on met l'image dans un header ou on dit le type de l'image
 
-            $redColor = imagecolorallocate($image,255,0,0);
-            imagefilledellipse($image,200,100,150,150,$redColor);
+        $image = imagecreatetruecolor(400,200); // on creer une image de 400 par 200
+        $blueBackground = imagecolorallocate($image,0,0 ,255); // on creer une couleur
+        // avec une image, puis les code pour une couleur, dans ce cas le bleu
 
-            header('Content-Type: image/jpeg');
-            imagejpeg($image);
-            
-        }
+        imagefill($image,0,0, $blueBackground); // on rempli l'image avec une couleur tel que :
+        // l'image de base, la position x, y puis la couleur
+
+        $redColor = imagecolorallocate($image,255,0,0); // on creer une couleur (rouge)
+        imagefilledellipse($image,200,100,150,150,$redColor); // on creer une elipse rempli d'une couleur tel que :
+        // l'image a laquelle on l'ajoute, la position x, y puis la largeur ensuite la hauteur pour finir sur la couleur
+
+        $textColor = imagecolorallocate($image,255,255,255); // on recreer une couleur (blanche) pour le texte
+        imagestring($image, 3, 50, 150, 'I AM TC AND I LOVE PHP', $textColor); // on ajoute le texte avec 
+        // d'abord, l'image puis la taille de la police (de 1 a 5) puis les coordonnées x, y, le message et la couleur
+
+        imagejpeg($image); // sauvegarde de l'image
+
+        // Sauvegarde l'image sur le disque
+        $pathToSave = 'image.jpg'; // le chemin de l'image
+        imagejpeg($image, $pathToSave); // ajout du chemin en argument de la methode (aussi simple que ça)
     }
-    ImageGenerator::generateImage();
+}
+    ImageGenerator::generateImage(); // appel de la methode
 ?>

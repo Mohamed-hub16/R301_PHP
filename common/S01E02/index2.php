dcf33979e87bdcd24b3c5c8d75cfb5ddcc1ee8bc
EthanLACHAUD <ethan.lachaud@etu.unilim.fr>
2023-12-22 09:37:32 +0100

    réglage index

--- common/S01E02/index2.php
+++ common/S01E02/index2.php
@@ -10,9 +10,8 @@
 <div id="centre">
 
     <ul>
-        <li><a href="common/S01E01/index.php">S01E01</a></li>
-        <li><a href="common/S01E02/index2.php">S01E02</a></li>
-        <li><a href="common/S01E03/index.php">S01E03</a></li>
+        <li><a href="index.php">Index</a></li>
+        <li><a href="sortie.html">Reproduction Google</a></li>
     </ul>
 
 </div>
--- common/S01E03/index.php
+++ common/S01E03/index.php
@@ -10,9 +10,8 @@
 <div id="centre">
 
     <ul>
-        <li><a href="common/S01E01/index.php">S01E01</a></li>
-        <li><a href="common/S01E02/index2.php">S01E02</a></li>
-        <li><a href="common/S01E03/index.php">S01E03</a></li>
+        <li><a href="index3.php">Base</a></li>
+        <li><a href="s01e03.php">Séparation</a></li>
     </ul>
 
 </div>
